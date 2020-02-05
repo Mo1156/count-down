1939505a601aeb6523b613311f51984f2d235518
Mo1156 <58610705+Mo1156@users.noreply.github.com>
2020-02-05 13:52:00 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+// create a for loop
+for (let index = 0; index <= 3; index++) {
+    game.splash("counting up:" + index)
+}
+// create a for loop that counts to 10 
+for (let index = 0; index <= 10; index++) {
+    game.splash("counting up:" + index)
+}
+// create a for loop that counts down 100 to 0 
+for (let index = 100; index >= 0; index--) {
+    game.splash("counting down:" + index)
+}
+// create a for loop that counts up 100 skiping 10 numbers each time 
+for (let index = 100; index <= 0; index-=10) {
+    game.splash("counting up by 10:" + index)
+}
